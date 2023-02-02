0f1a9aaceb6a31336df69877d836a1c449352dbc
nigarixx <105124936+nigarixx@users.noreply.github.com>
2023-02-02 19:45:47 +0400

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {  
+    //int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int i;
+    for (int i = 0; i < 11; i++)
+    {
+        printf("%i\n", i);
+    }
+}
